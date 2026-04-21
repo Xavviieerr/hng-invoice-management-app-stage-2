ff6a65fdeb747c5bf184354f4f80bf5f0fbd9d63
Chidera paul ogbu <ogbupaul545@gmail.com>
2026-04-21 13:12:45 +0100

    feat: add side bar component ui

--- src/app/AppLayout.jsx
+++ src/app/AppLayout.jsx
@@ -1,18 +1,40 @@
 import { useTheme } from "../context/themeContext";
 import LightDarkButton from "../shared/invoice/LightDarkButton";
+import Logo from "../assets/images/logo.png";
+import user from "../assets/images/user.png";
 
 export default function AppLayout({ children }) {
 	const { theme } = useTheme();
 
 	return (
 		<div className={`${theme}`}>
-			<div className="body">
-				{/* <LightDarkButton /> */}
+			<div className="body flex">
+				{/* Navigation side bar */}
+				<div className="w-[103px] bg-[#373B53] flex flex-col justify-between text-light-text-muted ring rounded-tr-3xl rounded-br-3xl h-screen">
+					{/* top section */}
+					<div className="h-[103px] bg-purple-primary rounded-br-3xl rounded-tr-3xl">
+						<div className="h-1/2"></div>
+						<img
+							src={Logo}
+							alt="Logo"
+							className="absolute top-8 justify-self-center"
+						/>
+						<div className="bg-purple-light w-full h-1/2 rounded-tl-3xl rounded-br-3xl"></div>
+					</div>
 
-				<div className="w-[103px] text-light-text-muted ring rounded-tr-2xl rounded-br-2xl h-screen">
-					sidebar
+					{/* bottom section */}
+					<div className="flex  flex-col gap-7  align-items-center">
+						<div className="flex flex-row justify-center">
+							<LightDarkButton />
+						</div>
+						<div className="rounded-br-3xl justify-items-center py-7 border-t border-t-light-text-muted">
+							<img src={user} alt="user Icon" />
+						</div>
+					</div>
 				</div>
-				{/* <main>{children}</main> */}
+
+				{/* main content area */}
+				<main>{children}</main>
 			</div>
 		</div>
 	);
--- src/shared/invoice/LightDarkButton.jsx
+++ src/shared/invoice/LightDarkButton.jsx
@@ -1,23 +1,18 @@
 import { useTheme } from "../../context/themeContext";
+import DarkModeIcon from "../../assets/images/darkModeButton.png";
+import LightModeIcon from "../../assets/images/lightModeButton.png";
 
 const LightDarkButton = () => {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<button
-			onClick={toggleTheme}
-			style={{
-				padding: "8px 16px",
-				cursor: "pointer",
-				borderRadius: "20px",
-				border: "1px solid #ccc",
-				backgroundColor: theme === "dark" ? "#444" : "#eee",
-				color: theme === "dark" ? "#fff" : "#000",
-				transition: "all 0.3s ease",
-			}}
-		>
+		<button onClick={toggleTheme} className="w-[20px] h-[20px]">
 			{/* Dynamic icon or text based on state */}
-			{theme === "dark" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+			{theme === "dark" ? (
+				<img src={DarkModeIcon} alt="Dark Mode Icon" />
+			) : (
+				<img src={LightModeIcon} alt="Light Mode Icon" />
+			)}
 		</button>
 	);
 };
